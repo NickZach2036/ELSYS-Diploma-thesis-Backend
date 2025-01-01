d7fdf4a14ff01ee5a2de3633e24fda690a9e3a34
Николай Захариев <nikolay.n.zahariev.2020@elsys-bg.org>
2025-01-01 21:09:30 +0200

    Finally successfully set up ESLint and Prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,53 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
 
+import pluginJs from '@eslint/js';
+
+import tseslint from 'typescript-eslint';
+
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+import prettierConfig from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
+
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+
+  {
+    languageOptions: {
+      globals: globals.node,
+
+      parserOptions: {
+        ecmaVersion: 2020,
+
+        sourceType: 'module',
+
+        project: './tsconfig.json',
+      },
+    },
+  },
+
   pluginJs.configs.recommended,
+
   ...tseslint.configs.recommended,
+
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+
+    rules: {
+      'prettier/prettier': [
+        'error',
+
+        {
+          singleQuote: true, // Ensure single quotes are enforced
+
+          endOfLine: 'crlf', // Use CRLF line endings
+        },
+      ],
+    },
+  },
+
+  prettierConfig, // Disable conflicting ESLint rules with Prettier
 ];
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'crlf', // Use CRLF line endings
+};
